0a2d5c8461cc706d3661c318f34ab12863d98ad1
John Grant <johnkgrant@gmail.com>
2021-01-17 13:20:47 -0500

    Add positive messages

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const date = new Date(Date.now())
-const dateElement = document.querySelector('#date')
-const displayOptions = {
-  weekday:'long',
-  month:'long',
-  day:'numeric'
+function showDate(){
+  const date = new Date(Date.now())
+  const dateElement = document.querySelector('#date')
+  const displayOptions = {
+    weekday:'long',
+    month:'long',
+    day:'numeric'
+  }
+
+  dateElement.textContent = date.toLocaleDateString('en-us', displayOptions)
 }
 
-dateElement.textContent = date.toLocaleDateString('en-us', displayOptions)
 
-/*
-*/
+function showPositiveMessage(){
+  fetch('messages.txt')
+    .then(response => response.text())
+    .then(text => {
+      const messageList = text.split('\n')
+      const messageNumber = Math.floor(Math.random()*messageList.length)
+      const messageElement = document.querySelector('#positiveMessage')
+      messageElement.textContent = messageList[messageNumber]
+    })
+}
 
-
+showDate()
+showPositiveMessage()
 
 
 

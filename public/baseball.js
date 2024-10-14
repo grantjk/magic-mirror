a38e14c3b7eb77ab149151c2b7986be6c20fb50d
John Grant <johnkgrant@gmail.com>
2024-10-14 07:33:54 -0700

    avoid showing old games

--- public/baseball.js
+++ public/baseball.js
@@ -7,78 +7,62 @@ export function getJaysSchedule() {
         const gameStart = moment(game.gameDate);
         const gameState = game.status.abstractGameState;
         const detailedState = game.status.detailedState;
+        const isRecentGame = gameStart.isAfter(moment().subtract(1, 'day'));
 
         console.log(`Start date: ${gameStart.toString()}`);
         console.log(gameState);
 
-        const awayTeam = game.teams.away.team.abbreviation;
-        const awayTeamId = game.teams.away.team.id;
-        const awayTeamScore = game.teams.away.score;
+        if (isRecentGame) {
+          const awayTeam = game.teams.away.team.abbreviation;
+          const awayTeamId = game.teams.away.team.id;
+          const awayTeamScore = game.teams.away.score;
 
-        const homeTeam = game.teams.home.team.abbreviation;
-        const homeTeamId = game.teams.home.team.id;
-        const homeTeamScore = game.teams.home.score;
+          const homeTeam = game.teams.home.team.abbreviation;
+          const homeTeamId = game.teams.home.team.id;
+          const homeTeamScore = game.teams.home.score;
 
-        const awayTeamRecord = game.teams.away.leagueRecord;
-        const awayTeamRecordDisplay = `${awayTeamRecord.wins} - ${awayTeamRecord.losses}`;
+          const awayTeamRecord = game.teams.away.leagueRecord;
+          const awayTeamRecordDisplay = `${awayTeamRecord.wins} - ${awayTeamRecord.losses}`;
 
-        const homeTeamRecord = game.teams.home.leagueRecord;
-        const homeTeamRecordDisplay = `${homeTeamRecord.wins} - ${homeTeamRecord.losses}`;
+          const homeTeamRecord = game.teams.home.leagueRecord;
+          const homeTeamRecordDisplay = `${homeTeamRecord.wins} - ${homeTeamRecord.losses}`;
 
-        document.querySelector("#away-team-logo").src = teamLogoUrl(awayTeamId);
-        document.querySelector("#home-team-logo").src = teamLogoUrl(homeTeamId);
+          document.querySelector("#away-team-logo").src = teamLogoUrl(awayTeamId);
+          document.querySelector("#home-team-logo").src = teamLogoUrl(homeTeamId);
 
-        document.querySelector("#away-team-name").textContent = awayTeam;
-        document.querySelector(
-          "#away-team-name-detail"
-        ).textContent = awayTeamRecordDisplay;
-        document.querySelector("#home-team-name").textContent = homeTeam;
-        document.querySelector(
-          "#home-team-name-detail"
-        ).textContent = homeTeamRecordDisplay;
+          document.querySelector("#away-team-name").textContent = awayTeam;
+          document.querySelector("#away-team-name-detail").textContent = awayTeamRecordDisplay;
+          document.querySelector("#home-team-name").textContent = homeTeam;
+          document.querySelector("#home-team-name-detail").textContent = homeTeamRecordDisplay;
 
-        document.querySelector("#home-team-name").textContent = homeTeam;
-        document.querySelector("#home-team-name").textContent = homeTeam;
+          document.querySelector("#game-status-text").textContent = detailedState;
+          if (gameStart.isAfter(moment())) {
+            // upcoming
+            document.querySelector("#game-status-text").textContent = "@";
+            document.querySelector("#game-status-subtext").textContent = gameStart.format("h:mm");
+            document.querySelector("#home-team-score").textContent = "";
+            document.querySelector("#away-team-score").textContent = "";
+          } else if (gameState === "Live") {
+            // in progress
+            document.querySelector("#home-team-score").textContent = homeTeamScore;
+            document.querySelector("#away-team-score").textContent = awayTeamScore;
 
-        document.querySelector("#game-status-text").textContent = detailedState;
-        if (gameStart.isAfter(moment())) {
-          // upcoming
-          document.querySelector("#game-status-text").textContent = "@";
-          document.querySelector(
-            "#game-status-subtext"
-          ).textContent = gameStart.format("h:mm");
-          document.querySelector("#home-team-score").textContent = "";
-          document.querySelector("#away-team-score").textContent = "";
-        } else if (gameState === "Live") {
-          // in progress
-          document.querySelector(
-            "#home-team-score"
-          ).textContent = homeTeamScore;
-          document.querySelector(
-            "#away-team-score"
-          ).textContent = awayTeamScore;
+            const inning = game.linescore.currentInningOrdinal;
+            const state = game.linescore.inningState;
+            const outs = game.linescore.outs;
+            document.querySelector("#game-status-text").textContent = `${state} ${inning}`;
 
-          const inning = game.linescore.currentInningOrdinal;
-          const state = game.linescore.inningState;
-          const outs = game.linescore.outs;
-          document.querySelector(
-            "#game-status-text"
-          ).textContent = `${state} ${inning}`;
-
-          const outText = outs < 3 ? `${outs} out` : "";
-          document.querySelector("#game-status-subtext").textContent = outText;
+            const outText = outs < 3 ? `${outs} out` : "";
+            document.querySelector("#game-status-subtext").textContent = outText;
+          } else {
+            // complete
+            document.querySelector("#home-team-score").textContent = homeTeamScore;
+            document.querySelector("#away-team-score").textContent = awayTeamScore;
+            document.querySelector("#game-status-subtext").textContent = "";
+          }
         } else {
-          // complete
-          document.querySelector(
-            "#home-team-score"
-          ).textContent = homeTeamScore;
-          document.querySelector(
-            "#away-team-score"
-          ).textContent = awayTeamScore;
-          document.querySelector("#game-status-subtext").textContent = "";
+          console.log("NO GAME!");
         }
-      } else {
-        console.log("NO GAME!");
       }
     });
 }
